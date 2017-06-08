d74b90471aaf946f6915882f65411df350b86502
yoges23 <yogivpy23@gmail.com>
2017-06-08 18:01:49 +0530

    Create Helo.java

---
+++ Helo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Helo
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int i;
+for(i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
