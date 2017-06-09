77774fb9cc3797e613749984142bcfd96abd1fa6
yoges23 <yogivpy23@gmail.com>
2017-06-09 15:04:57 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+public class Reverse
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer s=new StringBuffer("hello");
+		StringBuffer b=s.reverse();
+		System.out.println(b);
+	}
+}
